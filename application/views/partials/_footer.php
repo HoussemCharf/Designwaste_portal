115b897a4af279567227bce72f186a6940be5316
HoussemCharf <houssem.charf@gmail.com>
2018-04-23 00:35:05 +0100

    fixing footer

--- application/views/partials/_footer.php
+++ application/views/partials/_footer.php
@@ -130,9 +130,9 @@
         </div>
         <!-- .row -->
 <div style="display: block;">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Logotipo_de_la_Xunta_de_Galicia.svg/1280px-Logotipo_de_la_Xunta_de_Galicia.svg.png" width="350" height="200" style="display: inline;" />
+    <img src="https://i.imgur.com/cc2GuCr.png" width="200" height="200" style="display: inline;" />
     <img src="http://eupa.org.mt/wp-content/uploads/sites/11/2017/07/logo-erasmus-plus.png" width="350" height="200" style="display: inline;"/>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Logotipo_de_la_Xunta_de_Galicia.svg/1280px-Logotipo_de_la_Xunta_de_Galicia.svg.png" width="350" height="200" style="display: inline;"/>
+    <img src="https://i.imgur.com/cjDUzaE.png" width="400" height="200" style="display: inline;"/>
 </div>
         <!-- Copyright -->
         <div class="footer-bottom">
