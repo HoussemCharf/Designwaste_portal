5026658e09130e93acaddac5e82423d7a657d5b2
HoussemCharf <houssem.charf@gmail.com>
2018-04-22 23:26:09 +0100

    fixing footer

--- application/views/partials/_footer.php
+++ application/views/partials/_footer.php
@@ -130,10 +130,10 @@
         </div>
         <!-- .row -->
 <div style="display: block;">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Logotipo_de_la_Xunta_de_Galicia.svg/1280px-Logotipo_de_la_Xunta_de_Galicia.svg.png" width="200" height="200" style="display: inline;" />
-    <img src="http://eupa.org.mt/wp-content/uploads/sites/11/2017/07/logo-erasmus-plus.png" width="200" height="200" style="display: inline;"/>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Logotipo_de_la_Xunta_de_Galicia.svg/1280px-Logotipo_de_la_Xunta_de_Galicia.svg.png" width="200" height="200" style="display: inline;"/>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Logotipo_de_la_Xunta_de_Galicia.svg/1280px-Logotipo_de_la_Xunta_de_Galicia.svg.png" width="200" height="200" style="display: inline;"/>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Logotipo_de_la_Xunta_de_Galicia.svg/1280px-Logotipo_de_la_Xunta_de_Galicia.svg.png" width="350" height="200" style="display: inline;" />
+    <img src="http://eupa.org.mt/wp-content/uploads/sites/11/2017/07/logo-erasmus-plus.png" width="350" height="200" style="display: inline;"/>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Logotipo_de_la_Xunta_de_Galicia.svg/1280px-Logotipo_de_la_Xunta_de_Galicia.svg.png" width="350" height="200" style="display: inline;"/>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Logotipo_de_la_Xunta_de_Galicia.svg/1280px-Logotipo_de_la_Xunta_de_Galicia.svg.png" width="350" height="200" style="display: inline;"/>
 
         <!-- Copyright -->
         <div class="footer-bottom">
