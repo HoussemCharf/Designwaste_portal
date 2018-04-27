1bc779b3bfb7e957690db01596618364b1297cb9
HoussemCharf <houssem.charf@gmail.com>
2018-04-27 12:28:23 +0100

    restyling google translate widget to better fit with current theme

--- application/views/partials/_header.php
+++ application/views/partials/_header.php
@@ -89,6 +89,14 @@
     </script>
     <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
     </script>
+    <style>
+    .goog-te-gadget-simple {
+        font-size: none !important;
+        margin-top:9px !important;
+        padding-top:none !important;
+        padding-bottom: none !important;
+    }    
+</style>
 </head>
 <body>
 
