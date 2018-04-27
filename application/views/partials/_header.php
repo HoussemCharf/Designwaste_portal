17b84e6bb9322454bd8de2e7f742e5853475f788
HoussemCharf <houssem.charf@gmail.com>
2018-04-27 09:32:59 +0100

    adding french to the translation option and fixing copyright year

--- application/views/partials/_header.php
+++ application/views/partials/_header.php
@@ -84,7 +84,7 @@
     </script>
     <script type="text/javascript">
         function googleTranslateElementInit() {
-            new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,es,it', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+            new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,es,it,fr', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
         }
     </script>
     <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
